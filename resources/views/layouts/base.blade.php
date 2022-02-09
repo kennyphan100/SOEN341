200ab1a952063fb1716239a4e7fef5e4702b142d
Kenny Phan <kennyphan100@gmail.com>
2022-02-09 16:39:02 -0500

    #45 Fix CSS bug on deployed website

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,8 +11,8 @@
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
       <!-- External CSS -->
-      <link rel="stylesheet" type="text/css" href="{{ url('css/styles.css') }}">
-      <link rel="stylesheet" href="{{ url('css/responsive.css') }}">
+      <link rel="stylesheet" href="/css/styles.css">
+      <link rel="stylesheet" href="/css/responsive.css">
 
       <!-- Font Awesome -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -42,7 +42,7 @@
             <div class="container">
                <div class="row">
                   <div class="col-sm-12">
-                     <div class="logo"><a href="#">Amazoon</a></div>
+                     <div class="logo"><a href="/">Amazoon</a></div>
                   </div>
                </div>
             </div>
