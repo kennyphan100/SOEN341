3f1654ae69b48e72f1f6295089b081d8b36b4e77
Kenny Phan <kennyphan100@gmail.com>
2022-02-12 22:53:26 -0500

    #64 Fix CSS on products page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -12,46 +12,46 @@
   <div class="row 1 row-2-cols-3">
     <div class="col">
       <a href=""><img src="images/twilight.jpg"></a> <br>
-      <div class="genre1">Fantasy / Romance</div>
-      <a href=""><h2 class="name1">Twilight</h2></a>
-      <div class="price1">$9.99</div> <br><br>
+      <div class="product_description">Fantasy / Romance</div>
+      <h2 class="product_name"><a href="">Twilight</a></h2>
+      <div class="product_price">$9.99</div> <br><br>
     </div>
 
     <div class="col">
       <a href=""><img src="images/the_da_vinci_code.jpg"></a> <br>
-      <div class="genre1">Mystery Thriller</div>
-      <a href=""><h2 class="name1">The Da Vinci Code</h2></a>
-      <div class="price1">$11.99</div> <br><br>
+      <div class="product_description">Mystery Thriller</div>
+      <h2 class="product_name"><a href="">The Da Vinci Code</a></h2>
+      <div class="product_price">$11.99</div> <br><br>
     </div>
 
     <div class="col">
       <a href=""><img src="images/infinity_gauntlet.jpg"></a> <br>
-      <div class="genre1">Superhero</div>
-      <a href=""><h2 class="name1">The Infinity Gauntlet</h2></a>
-      <div class="price1">$38.99</div> <br><br>
+      <div class="product_description">Superhero</div>
+      <h2 class="product_name"><a href="">The Infinity Gauntlet</a></h2>
+      <div class="product_price">$38.99</div> <br><br>
     </div>
   </div>
 
   <div class="row 2 row-2-cols-3">
     <div class="col">
       <a href=""><img src="images/fifty_shades_of_grey.jpg"></a> <br>
-      <div class="genre1">Romance</div>
-      <a href=""><h2 class="name1">Fifty Shades of Grey</h2></a>
-      <div class="price1">$13.99</div> <br>
+      <div class="product_description">Romance</div>
+      <h2 class="product_name"><a href="">Fifty Shades of Grey</a></h2>
+      <div class="product_price">$13.99</div> <br>
     </div>
 
     <div class="col">
       <a href=""><img src="images/sorcerers_stone.jpg"></a> <br>
-      <div class="genre1">Fantasy / Adventure</div>
-      <a href=""><h2 class="name1">Harry Potter and The Sorcerer's Stone</h2></a>
-      <div class="price1">$16.99</div> <br>
+      <div class="product_description">Fantasy / Adventure</div>
+      <h2 class="product_name"><a href="">Harry Potter and The Sorcerer's Stone</a></h2>
+      <div class="product_price">$16.99</div> <br>
     </div>
 
     <div class="col">
       <a href=""><img src="images/a_game_of_thrones.jpg"></a> <br>
-      <div class="genre1">Political Fiction</div>
-      <a href=""><h2 class="name1">A Game of Thrones</h2></a>
-      <div class="price1">$13.99</div> <br>
+      <div class="product_description">Political Fiction</div>
+      <h2 class="product_name"><a href="">A Game of Thrones</a></h2>
+      <div class="product_price">$13.99</div> <br>
     </div>
   </div>
 
