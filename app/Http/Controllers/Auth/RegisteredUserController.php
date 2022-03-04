6b77e5d8bd3fa9fc8cababf9235ce3782b4ce993
Kenny Phan <kennyphan100@gmail.com>
2022-03-04 17:47:01 -0500

    #65 Save the account type to database

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'account_type' => $request->account_type
         ]);
 
         event(new Registered($user));
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,10 +48,10 @@
 
             <!-- User or Admin Account -->
             <div class="mt-4">
-                <x-label for="userType" :value="__('Is this a User or Admin account?')" />
-                <select name="userType" id="userType" class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" require>
-                    <option value="User">User</option>
-                    <option value="Admin">Admin</option>
+                <x-label for="account_type" :value="__('Is this a User or Admin account?')" />
+                <select name="account_type" id="account_type" class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" require>
+                    <option value="user">User</option>
+                    <option value="admin">Admin</option>
                 </select>
             </div>
             
