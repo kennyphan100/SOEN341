473792fdfeda2f80a1a07eb9581a5d1f6d58e955
Thamina Akther <thamina_akther@hotmail.com>
2022-03-06 02:18:58 -0500

    #79 testing next the blade in blade

--- resources/views/userProfile/show.blade.php
+++ resources/views/userProfile/show.blade.php
@@ -4,38 +4,32 @@
 
 @section('content')
 
-<!-- <link rel="stylesheet" href="/css/products.css"> -->
+<link rel="stylesheet" href="/css/userProfile.css">
 
-<h1 class="title">{{ $user_name }}</h1><br>
+<h1 class="title">{{ $user_name }}'s Profile</h1><br>
 
 <div class="grid-container">
-  <div class="list-group" class="s341-filter-option-sidebar">
-    <ul>
-      <li class="s341-filter-item-header"><button type="button" class="btn btn-link" disabled>Price Filters</button></li>
-      <ul>
-        <li class="s341-filter-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'asc']) }}"><button type="button" class="btn btn-link">Price Low-High</button></a></li>
-        <li class="s341-filter-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Price High-Low</button></a></li>
-      </ul>
-      <li class="s341-filter-item-header"><button type="button" class="btn btn-link" disabled>Rating Filters</button></li>
-      <ul>
-        <li class="s341-filter-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'asc']) }}"><button type="button" class="btn btn-link">Rating Low-High</button></a></li>
-        <li class="s341-filter-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Rating High-Low</button></a></li>
-      </ul>
-    </ul>
-  </div>
-  <div class="container">
-
-    @if(session('status'))
-    <div class="alert alert-success">
-      {{ session('status') }}
+    <div class="list-group" class="s341-user-profile-option-sidebar">
+        <ul>
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
+            
+            <!--             
+            <li class="s341-user-profile-item-header"><button type="button" class="btn btn-link" disabled>Price Filters</button></li>
+            <ul>
+                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Price High-Low</button></a></li>
+            </ul>
+            <li class="s341-user-profile-item-header"><button type="button" class="btn btn-link" disabled>Rating Filters</button></li>
+            <ul>
+                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'asc']) }}"><button type="button" class="btn btn-link">Rating Low-High</button></a></li>
+                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Rating High-Low</button></a></li>
+            </ul>
+         -->
+        </ul>
     </div>
-    @endif
-
-    <div class="row 1 row-2-cols-3">
-
+    <div class="container">
+        <div class="row 1 row-2-cols-3">
+        </div>
     </div>
-
-  </div>
 </div>
 
-  @endsection
+@endsection
---
+++ resources/views/userProfile/userProfileSidebar.blade.php
@@ -0,0 +1,8 @@
+    <div class="list-group" class="s341-user-profile-option-sidebar">
+        <ul>
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
+
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
+
+        </ul>
+    </div>
