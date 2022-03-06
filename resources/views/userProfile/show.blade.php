66750d611999b519c743c945b2325f20434c0b68
Thamina Akther <thamina_akther@hotmail.com>
2022-03-06 02:37:28 -0500

    #79 Added user profile sidebar as a separate view

--- resources/views/userProfile/show.blade.php
+++ resources/views/userProfile/show.blade.php
@@ -9,26 +9,11 @@
 <h1 class="title">{{ $user_name }}'s Profile</h1><br>
 
 <div class="grid-container">
-    <div class="list-group" class="s341-user-profile-option-sidebar">
-        <ul>
-            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
-            
-            <!--             
-            <li class="s341-user-profile-item-header"><button type="button" class="btn btn-link" disabled>Price Filters</button></li>
-            <ul>
-                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Price High-Low</button></a></li>
-            </ul>
-            <li class="s341-user-profile-item-header"><button type="button" class="btn btn-link" disabled>Rating Filters</button></li>
-            <ul>
-                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'asc']) }}"><button type="button" class="btn btn-link">Rating Low-High</button></a></li>
-                <li class="s341-user-profile-items"><a href="{{ route('categories.show_sort_by_price', [$user_name, 'desc']) }}"><button type="button" class="btn btn-link">Rating High-Low</button></a></li>
-            </ul>
-         -->
-        </ul>
-    </div>
+
+    @include('userProfile.userProfileSidebar')
+    
     <div class="container">
-        <div class="row 1 row-2-cols-3">
-        </div>
+
     </div>
 </div>
 
--- resources/views/userProfile/userProfileSidebar.blade.php
+++ resources/views/userProfile/userProfileSidebar.blade.php
@@ -1,8 +1,10 @@
     <div class="list-group" class="s341-user-profile-option-sidebar">
         <ul>
-            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">Info</button></a></li>
 
-            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">User Info</button></a></li>
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">Orders</button></a></li>
+
+            <li class="s341-user-profile-items"><a href="{{ route('userProfile.show_user_information') }}"><button type="button" class="btn btn-link">Payments</button></a></li>
 
         </ul>
     </div>
