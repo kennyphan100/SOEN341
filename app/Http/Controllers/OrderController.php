d7caa7a02073256b4fa6daabc7acf59b1561b47f
Steven Franchetto <steven.franchetto@gmail.com>
2022-03-11 18:54:58 -0500

    #86 Created two tables for checkout functionality

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use App\Models\Order;
+use App\Models\Order_Product;
 
 class OrderController extends Controller
 {
@@ -29,23 +30,27 @@ class OrderController extends Controller
             $nb_of_items += $product->qty;
         }
 
+        $order = new Order;
+        $order -> user_Id = $user->id;
+        $order -> city = $req->city;
+        $order -> shipping_address = $req->address;
+        $order -> postal_code = $req->zip;
+        $order -> credit_card_number = $req->cc_number;
+        $order -> name_on_card = $req->cc_name;
+        $order -> cc_security_code = $req->cc_cvv;
+        $order -> order_total = $subtotal;
+        $order -> cancelled = "NO";
+        $order->save();
+
+        $order_id = $order->id; //gets last id inserted
+
         foreach($products_in_cart as $cart)
         {
-            $order = new Order;
-            $order -> user_Id = $user->id;
-            $order -> product_id = $cart->id;
-            $order -> firstname = $req->firstName;
-            $order -> lastname = $req->lastName;
-            $order -> address = $req->address;
-            $order -> address2 = $req->address2;
-            $order -> country = $req->country;
-            $order -> state_province = $req->province;
-            $order -> zipcode = $req->zip;
-            $order -> payment_method = $req->paymentMethod;
-            $order -> credit_card_number = $req->cc_number;
-            $order -> status = "pending";
-            $order -> totalprice = $subtotal;
-            $order->save();
+            $order_product = new Order_Product;
+            $order_product -> order_id = $order_id;
+            $order_product -> product_id = $cart->id;
+            $order_product -> product_quantity = $cart->qty;
+            $order_product->save();
         }
         
         Cart::destroy(); //empties the cart
